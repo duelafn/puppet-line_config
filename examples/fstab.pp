69525cdfcd4fc100f4e9015049dbea59cae73155
Dean Serenevy <dean@serenevy.net>
2012-11-17 13:23:13 -0500

    examples: forgot to accept/pass ensure property through

--- examples/fstab.pp
+++ examples/fstab.pp
@@ -1,6 +1,7 @@
 
-define fstab($mount=$name, $device, $type, $options=defaults, $dump=0, $pass=0) {
+define fstab($mount=$name, $device, $type, $ensure=present, $options=defaults, $dump=0, $pass=0) {
     file_line { "/etc/fstab: $mount":
+        ensure => $ensure,
         path => "/etc/fstab",
         content => "$device   $mount   $type   $options    $dump    $pass",
         replace  => "\\s$mount\\s",
